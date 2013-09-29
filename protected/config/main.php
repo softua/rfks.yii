f06ac7716d62bd41f7601facd929ff1a34747eb2
softua <softua6@gmail.com>
2013-09-29 13:53:43 +0300

    меняю array() на []

--- protected/config/main.php
+++ protected/config/main.php
@@ -5,21 +5,21 @@
 
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
-return array(
+return [
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Ассоциация компьютерного спорта',
 	'language'=>'ru',
 
 	// preloading 'log' component
-	'preload'=>array('log'),
+	'preload'=>['log'],
 
 	// autoloading model and component classes
-	'import'=>array(
+	'import'=>[
 		'application.models.*',
 		'application.components.*',
-	),
+	],
 
-	'modules'=>array(
+	'modules'=>[
 		// uncomment the following to enable the Gii tool
 		/*
 		'gii'=>array(
@@ -29,62 +29,60 @@ return array(
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
 		*/
-	),
+	],
 
 	// application components
-	'components'=>array(
-		'user'=>array(
+	'components'=>[
+		'user'=>[
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
-		),
+		],
 		// uncomment the following to enable URLs in path-format
-		'urlManager'=>array(
+		'urlManager'=>[
 			'urlFormat'=>'path',
 			'showScriptName'=>false,
-			'rules'=>array(
+			'rules'=>[
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-			),
-		),
-		'db'=>array(
+			],
+		],
+		'db'=>[
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
+		],
 		// uncomment the following to use a MySQL database
 		/*
-		'db'=>array(
+		'db'=>[
 			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
 			'charset' => 'utf8',
-		),
+		],
 		*/
-		'errorHandler'=>array(
+		'errorHandler'=>[
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
-		),
-		'log'=>array(
+		],
+		'log'=>[
 			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
+			'routes'=>[
+				[
 					'class'=>'CFileLogRoute',
 					'levels'=>'error, warning',
-				),
+				],
 				// uncomment the following to show log messages on web pages
-				/*
-				array(
+				[
 					'class'=>'CWebLogRoute',
-				),
-				*/
-			),
-		),
-	),
+				],
+			],
+		],
+	],
 
 	// application-level parameters that can be accessed
 	// using Yii::app()->params['paramName']
-	'params'=>array(
+	'params'=>[
 		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
-	),
-);
+		'adminEmail'=>'softua6@gmail.com',
+	],
+];
